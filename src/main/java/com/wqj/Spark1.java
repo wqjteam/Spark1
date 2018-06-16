046c15ea6406b7c5ea390636c3d7395b70353090
githubwqj2 <2812930196@qq.com>
2018-06-16 21:29:54 +0800

    笔记本提交

--- src/main/java/com/wqj/Spark1.java
+++ src/main/java/com/wqj/Spark1.java
@@ -5,7 +5,7 @@ package com.wqj;
  * @Date: 2018/6/15 16:15
  * @Description:
  */
-public class spark {
+public class Spark1 {
     public static void main(String[] args) {
         System.out.println("这是一个spark的文件");
     }
