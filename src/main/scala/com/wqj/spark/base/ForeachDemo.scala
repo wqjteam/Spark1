edcec91469cbc8600429d93595a40e2ebbe1d4e5
githubwqj <893453172@qq.com>
2018-06-16 18:47:38 +0800

    台式机提交

--- src/main/scala/com/wqj/spark/base/ForeachDemo.scala
+++ src/main/scala/com/wqj/spark/base/ForeachDemo.scala
@@ -1,10 +1,22 @@
 package com.wqj.spark.base
 
+import org.apache.spark.{SparkConf, SparkContext}
+
 /**
   * @Auther: wqj
   * @Date: 2018/6/16 18:40
   * @Description:
   */
 object ForeachDemo {
+  def main(args: Array[String]): Unit = {
+    //local[2]代表两个线程
+    val conf =new SparkConf().setAppName("ForeachDemo").setMaster("local")
+    val sc =new SparkContext(conf)
+    val rdd1 =sc.parallelize(List(1,2,3,4,5,6,7,8,9))
+    rdd1.foreach(x=>print(_))
+
+    //业务上可以添加至数据库中
+    sc.stop()
+  }
 
 }
