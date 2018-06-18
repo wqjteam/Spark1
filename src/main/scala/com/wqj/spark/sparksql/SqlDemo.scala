5ab319ccd13706197b592746f1429f07a74fc164
githubwqj2 <2812930196@qq.com>
2018-06-18 23:10:19 +0800

    笔记本提交

---
+++ src/main/scala/com/wqj/spark/sparksql/SqlDemo.scala
@@ -0,0 +1,34 @@
+package com.wqj.spark.sparksql
+
+import org.apache.spark.sql.{SQLContext, SparkSession}
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * @Auther: wqj
+  * @Date: 2018/6/18 22:47
+  * @Description:
+  */
+object SqlDemo {
+  def main(args: Array[String]): Unit = {
+
+
+
+  val conf = new SparkConf().setAppName("SqlDemo").setMaster("local[2]")
+  val sc = new SparkContext(conf);
+  val sqlContext = new SQLContext(sc)
+  val rdd1 = sc.parallelize(Array(("zhangsan", 1), ("lisi", 2), ("wangwu", 3), ("zhaoliu", 4), ("liqi", 5)))
+  val rdd2 = rdd1.map(x => {
+    Person(x._1, x._2)
+  })
+
+  import sqlContext.implicits._
+
+  val df1= rdd2.toDF()
+  df1.registerTempTable("person_table")
+    println(sqlContext.sql("select * from person_table ").show())
+//  println(sqlContext.sql("select * from person_table order by age desc limit 2").show())
+  }
+}
+
+
+case class Person(name: String, age: Int)
