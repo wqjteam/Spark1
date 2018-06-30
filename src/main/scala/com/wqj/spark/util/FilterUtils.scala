9e8d5444b3a3f91361b380edb38830796a3041ec
githubwqj2 <2812930196@qq.com>
2018-06-30 17:22:17 +0800

    笔记本提交

---
+++ src/main/scala/com/wqj/spark/util/FilterUtils.scala
@@ -0,0 +1,29 @@
+package com.wqj.spark.util
+
+import java.text.SimpleDateFormat
+
+import org.apache.commons.lang3.time.FastDateFormat
+
+
+
+
+/**
+  * @Auther: wqj
+  * @Date: 2018/6/30 16:45
+  * @Description:
+  */
+object FilterUtils {
+
+  val sdf=new FastDateFormat("yyyy年MM月dd日,E,HH:MM:ss")
+  def filterByTime(fields:Array[String],startTime:Long,endTime: Long):Boolean={
+//val sdf=new SimpleDateFormat("yyyy年MM月dd日,E,HH:MM:ss")
+    val time=sdf.parse(fields(1)).getTime
+    if (time<=endTime&&time>startTime){
+      true
+    }else{
+      false
+    }
+
+  }
+
+}
