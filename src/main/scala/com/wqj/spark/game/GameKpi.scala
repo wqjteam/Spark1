e44e3f5dfeacc65aa29fbb45775efed475f8d88a
githubwqj2 <2812930196@qq.com>
2018-06-30 18:15:11 +0800

    笔记本提交

--- src/main/scala/com/wqj/spark/game/GameKpi.scala
+++ src/main/scala/com/wqj/spark/game/GameKpi.scala
@@ -1,5 +1,6 @@
 package com.wqj.spark.game
 
+import com.wqj.spark.common.EventType
 import com.wqj.spark.util.{FilterUtils, TimeUtils}
 import org.apache.spark.{SparkConf, SparkContext}
 
@@ -15,8 +16,19 @@ object GameKpi {
   def main(args: Array[String]): Unit = {
     val conf=new SparkConf().setAppName("GameKpi").setMaster("local[3]")
     val sc=new SparkContext(conf)
-    val text=sc.textFile("D:\\input\GameLog.txt").map(_.split("\\|"))
+    val text=sc.textFile("D:/input/GameLog.txt").map(_.split("\\|"))
     //筛选日志之后的数据
     val cacheDate=text.filter(x=>FilterUtils.filterByTime(x,TimeUtils.DateStringToLong("2015/6/30 15:51:11"),TimeUtils.DateStringToLong("2018/6/30 15:51:11"))).cache()
+
+    //日新增用户,register只会有一次可以直接count
+    val dnu=cacheDate.filter(x=>{
+      x(0).equals(EventType.REGISTER)
+    }).count()
+
+
+    //计算日活跃用户
+
+    print(dnu)
+
   }
 }
