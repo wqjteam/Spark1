bb2ef1ccc8831853f22df7eb13580ffbed548ea4
githubwqj2 <2812930196@qq.com>
2018-07-01 14:06:34 +0800

    笔记本提交

--- src/main/scala/com/wqj/spark/util/TimeUtils.scala
+++ src/main/scala/com/wqj/spark/util/TimeUtils.scala
@@ -1,6 +1,9 @@
 package com.wqj.spark.util
 
+import java.sql.Date
 import java.text.SimpleDateFormat
+import java.util
+import java.util.Calendar
 
 /**
   * @Auther: wqj
@@ -11,7 +14,20 @@ object TimeUtils {
 
   def DateStringToLong(dateString:String):Long={
 
-    val sdf=new SimpleDateFormat("yyyy/mm/dd HH:MM:ss")
+    val sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
     sdf.parse(dateString).getTime
+  }
+
+  def DateStringToDate(dateString:String):util.Date={
+
+    val sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+    sdf.parse(dateString)
+  }
+
+  def DateAddOrDel(date1:Long,num:Int):Long={
+    val cale=Calendar.getInstance()
+    cale.setTimeInMillis(date1)
+    cale.add(Calendar.DATE,num )
+    cale.getTime.getTime
   }
 }
