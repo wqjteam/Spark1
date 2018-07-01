209cb4bccce1683901a6fc54799afa10a195d1be
githubwqj2 <2812930196@qq.com>
2018-07-01 22:33:00 +0800

    笔记本提交

--- src/main/scala/com/wqj/spark/game/PluginScann.scala
+++ src/main/scala/com/wqj/spark/game/PluginScann.scala
@@ -2,6 +2,7 @@ package com.wqj.spark.game
 
 import java.util
 
+import com.wqj.spark.util.{RedisUtil, TimeUtils}
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.spark.streaming.{Seconds, StreamingContext}
@@ -16,29 +17,66 @@ import scala.collection.mutable
   */
 object PluginScann {
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName("PluginScann").setMaster("local[2]")
+    val conf = new SparkConf().setAppName("PluginScann").setMaster("local[3]")
     conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
     val sc = new SparkContext(conf)
     val ssc = new StreamingContext(sc, Seconds(10))
     sc.setCheckpointDir("e://checkpoint")
-    val map = Map(("test" -> 1));
-//    val kafkaParams = Map[String, String](
-//      "zookeeper.connect" -> "kafka:2181",
-//      "group.id" -> "local"
-//
-//      //      ,"auto.offset.reset" -> "smallest"
-//    )
-    val stream = KafkaUtils.createStream(ssc, "kafka:2181","local", map, StorageLevel.MEMORY_AND_DISK_SER)
-    //_.1为patitioner的下标_._2为partitioner中的数据
+    val map = Map(("test" -> 3));
+    //    val kafkaParams = Map[String, String](
+    //      "zookeeper.connect" -> "kafka:2181",
+    //      "group.id" -> "local"
+    //
+    //      //      ,"auto.offset.reset" -> "smallest"
+    //    )
+    val stream = KafkaUtils.createStream(ssc, "kafka:2181", "local", map, StorageLevel.MEMORY_AND_DISK_SER)
+    //_.1为patitioner的下标_._2为partitioner中的数据,发现第一个为null
     //@return DStream of (Kafka message key, Kafka message value)
     val lines = stream.map(x => {
-      println(x._1, x._2)
+      //      println(x._1, x._2)
       x._2
     })
-    lines.print()
+    val qxysData = lines.map(_.split("/t")).filter(x => {
+
+      x(3).eq("11") && x(8).equals("强效药水")
+    })
+
+    //按照窗口函数分好组的数据
+    val windowDate = qxysData.map(x => {
+      (x(7), x(12))
+    }).groupByKeyAndWindow(Seconds(30), Seconds(20))
+
+    //排除网络问题,排除5次使用的
+    val caledata = windowDate.filter(x => {
+      x._2.size > 5
+    })
+
+    //平均使用药水时长
+    val pjsysc = caledata.mapValues(x => {
+      val list = x.toList.sorted
+      val count = list.size
+      val time = TimeUtils.DateStringToLong(list(0)) - TimeUtils.DateStringToLong(list(count - 1))
+      count.toLong / time
+    })
+
+    //如果大于某个标准就是 开挂了
+    val kaiguadata = pjsysc.filter(x => {
+      x._2 < 1000
+    })
+    kaiguadata.foreachRDD(x=>{
+      x.foreachPartition(y=>{
+        val jedis=RedisUtil.getConnection()
+        jedis.select(3);
+        y.foreach(z=>{
+          print("进入到方法中2")
+          jedis.set(z._1,z._2.toString)
+        })
+      })
+    })
+
+    kaiguadata.print()
     ssc.start()
     ssc.awaitTermination()
-    print("11")
   }
 
 }
