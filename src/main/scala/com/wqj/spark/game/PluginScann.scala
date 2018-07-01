af897d0bae5ec78d419fe1752abbb557cd4cc13d
githubwqj2 <2812930196@qq.com>
2018-07-01 21:47:12 +0800

    笔记本提交

--- src/main/scala/com/wqj/spark/game/PluginScann.scala
+++ src/main/scala/com/wqj/spark/game/PluginScann.scala
@@ -16,22 +16,23 @@ import scala.collection.mutable
   */
 object PluginScann {
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName("PluginScann").setMaster("local[3]")
+    val conf = new SparkConf().setAppName("PluginScann").setMaster("local[2]")
     conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
     val sc = new SparkContext(conf)
     val ssc = new StreamingContext(sc, Seconds(10))
     sc.setCheckpointDir("e://checkpoint")
-    val map= Map(("test"-> 3));
-    val kafkaParams = Map[String, String](
-      "zookeeper.connect" -> "kafka:2181",
-      "group.id" -> "local",
-      "auto.offset.reset" -> "smallest"
-    )
-    val stream=KafkaUtils.createStream(ssc,kafkaParams ,map,StorageLevel.MEMORY_AND_DISK_SER)
+    val map = Map(("test" -> 1));
+//    val kafkaParams = Map[String, String](
+//      "zookeeper.connect" -> "kafka:2181",
+//      "group.id" -> "local"
+//
+//      //      ,"auto.offset.reset" -> "smallest"
+//    )
+    val stream = KafkaUtils.createStream(ssc, "kafka:2181","local", map, StorageLevel.MEMORY_AND_DISK_SER)
     //_.1为patitioner的下标_._2为partitioner中的数据
-   //@return DStream of (Kafka message key, Kafka message value)
-    val lines=stream.map(x=>{
-      println(x._1,x._2)
+    //@return DStream of (Kafka message key, Kafka message value)
+    val lines = stream.map(x => {
+      println(x._1, x._2)
       x._2
     })
     lines.print()
