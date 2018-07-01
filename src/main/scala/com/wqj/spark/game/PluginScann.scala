f6b57561ffef7770ac7e027639742c52d340edb5
githubwqj <893453172@qq.com>
2018-07-01 18:29:16 +0800

    台式机提交

---
+++ src/main/scala/com/wqj/spark/game/PluginScann.scala
@@ -0,0 +1,38 @@
+package com.wqj.spark.game
+
+import java.util
+
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.streaming.kafka.KafkaUtils
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.collection.mutable
+
+/**
+  * @Auther: wqj
+  * @Date: 2018/7/1 17:11
+  * @Description:
+  */
+object PluginScann {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("PluginScann").setMaster("local[3]")
+    conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+    val sc = new SparkContext(conf)
+    val ssc = new StreamingContext(sc, Seconds(10))
+    sc.setCheckpointDir("e://checkpoint")
+    val map= Map(("test"-> 3));
+    val stream=KafkaUtils.createStream(ssc, "kafka:2181", "local",map,StorageLevel.MEMORY_AND_DISK_SER)
+    //_.1为patitioner的下标_._2为partitioner中的数据
+   //@return DStream of (Kafka message key, Kafka message value)
+    val lines=stream.map(x=>{
+      println(x._1,x._2)
+      x._2
+    })
+    lines.print()
+    ssc.start()
+    ssc.awaitTermination()
+    print("11")
+  }
+
+}
