31a2d271f5c35ec1905f80dd77969d426ebface0
githubwqj <893453172@qq.com>
2018-07-26 15:10:16 +0800

    台式机提交

---
+++ src/main/java/com/wqj/JavaLru.java
@@ -0,0 +1,9 @@
+package com.wqj;
+
+/**
+ * @Auther: wqj
+ * @Date: 2018/7/26 10:59
+ * @Description:
+ */
+public class JavaLru {
+}
---
+++ src/main/java/com/wqj/LruCache.java
@@ -0,0 +1,58 @@
+package com.wqj;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @Auther: wqj
+ * @Date: 2018/7/26 15:09
+ * @Description:
+ */
+public class LruCache<K, V> {
+    private final int MAX_CACHE_SIZE;
+    private final float DEFAULT_LOAD_FACTORY = 0.75f;
+
+    LinkedHashMap<K, V> map;
+
+    public LruCache(int cacheSize) {
+        MAX_CACHE_SIZE = cacheSize;
+        int capacity = (int)Math.ceil(MAX_CACHE_SIZE / DEFAULT_LOAD_FACTORY) + 1;
+        /*
+         * 第三个参数设置为true，代表linkedlist按访问顺序排序，可作为LRU缓存
+         * 第三个参数设置为false，代表按插入顺序排序，可作为FIFO缓存
+         */
+        map = new LinkedHashMap<K, V>(capacity, DEFAULT_LOAD_FACTORY, false) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+                return size() > MAX_CACHE_SIZE;
+            }
+        };
+    }
+
+    public synchronized void put(K key, V value) {
+        map.put(key, value);
+    }
+
+    public synchronized V get(K key) {
+        return map.get(key);
+    }
+
+    public synchronized void remove(K key) {
+        map.remove(key);
+    }
+
+    public synchronized Set<Map.Entry<K, V>> getAll() {
+        return map.entrySet();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Map.Entry<K, V> entry : map.entrySet()) {
+            stringBuilder.append(String.format("%s: %s  ", entry.getKey(), entry.getValue()));
+        }
+        return stringBuilder.toString();
+    }
+
+}
