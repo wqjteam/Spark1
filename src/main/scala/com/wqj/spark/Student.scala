14260b91b7fb478756294de1fc61ffcb3c1c9374
githubwqj2 <2812930196@qq.com>
2018-08-05 21:11:19 +0800

    笔记本提交

---
+++ src/main/scala/com/wqj/spark/Student.scala
@@ -0,0 +1,12 @@
+package com.wqj.spark
+
+/**
+  * @Auther: wqj
+  * @Date: 2018/8/5 20:50
+  * @Description:
+  */
+class Student(var id: String,var name: String) {
+  def this() {
+    this("1","ad")
+  }
+}
--- src/main/scala/com/wqj/spark/Test.scala
+++ src/main/scala/com/wqj/spark/Test.scala
@@ -1,5 +1,7 @@
 package com.wqj.spark
 
+import com.fasterxml.jackson.databind.ObjectMapper
+
 import scala.collection.mutable.ArrayBuffer
 
 /**
@@ -7,11 +9,15 @@ import scala.collection.mutable.ArrayBuffer
   * @Date: 2018/6/17 10:49
   * @Description:
   */
+
 object Test extends App {
-
-  var a=ArrayBuffer[(Int,Int)]()
-  a+=((1,1))
-  println(a.toBuffer)
-
-  val c=1
+  val str="{\"id\":\"1\",\"name\":\"zs\"}"
+  val map=new ObjectMapper()
+  val student: Student = map.readValue(str,classOf[Student])
+  print(student.toString)
+//  var a=ArrayBuffer[(Int,Int)]()
+//  a+=((1,1))
+//  println(a.toBuffer)
+//
+//  val c=1
 }
