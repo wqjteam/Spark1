7c0ed2f42b2b773654360037cfde23e4fac7aa75
githubwqj2 <2812930196@qq.com>
2019-04-02 14:26:01 +0800

    笔记本提交

--- src/main/scala/com/wqj/spark/test/LogTest.scala
+++ src/main/scala/com/wqj/spark/test/LogTest.scala
@@ -1,6 +1,7 @@
 package com.wqj.spark.test
 
 import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 import org.bson.types.ObjectId
 
@@ -28,10 +29,12 @@ object LogTest {
       println("输出的随机id:"+new ObjectId())
 
     }
-//    val rdd1: RDD[String] = sparkSession.sparkContext.parallelize(Array("a", "b", "c", "d", "f"), 2)
+    val rdd1: RDD[String] = sparkSession.sparkContext.parallelize(Array("a", "b", "c", "d", "f","a","e","a","g"), 3)
 //    while (true) {
 //      log.error("test1的数据-------------------------------------" + rdd1.collect().toBuffer + "------------------------")
-//      Thread.sleep(1000)
+//      Thread.sleep(10)
 //    }
+
+    println("数据结果为"+rdd1.map(x=>(x,1)).groupByKey(2).collect().mkString)
   }
 }
